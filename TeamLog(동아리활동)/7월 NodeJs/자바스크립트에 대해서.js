eac5fc4a0ddf5c325264dec5416a7539c6e91a55
김우민 <mathasdf@naver.com>
2017-07-27 13:36:32 +0900

    class
    class

---
+++ TeamLog(동아리활동)/7월 NodeJs/자바스크립트에 대해서.js
@@ -0,0 +1,48 @@
+//클래스, 메소드 개념 설명
+
+/*
+class a{
+	ab(){
+		console.log('asdf');
+	}
+}
+
+var A = new a();
+
+A.ab()
+*/
+
+//사칙연산
+/*
+var dkv = 1, enl = 2;
+
+class dustks{
+	A(a,b){
+		console.log(a+b);
+	}
+	B(a,b){
+		console.log(a-b);
+	}
+	C(a,b){
+		console.log(a*b);
+	}
+	D(a,b){
+		console.log(a/b);
+	}
+}
+
+var app = new dustks();
+
+app.B(dkv,enl)
+*/ 
+
+a = [1,3,2];
+b = [4,6,5];
+
+function call(a,b){
+	return a.concat(b);
+}
+
+var result = call(a,b);
+
+console.log(result.sort());
